3d1a109246d600255c9309224e193bc728529990
Josep Subils <js@js.gl>
2020-03-31 21:13:25 +0200

    Initial Commit

---
+++ play1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  play1
+//
+//  Created by Josep Subils Rigau on 31/03/2020.
+//  Copyright © 2020 Josep Subils Rigau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
