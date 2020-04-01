e74c5e0cc073b967ae4029504d700742c2c1c37d
Josep Subils <js@js.gl>
2020-04-01 07:02:39 +0200

    play text

--- play1/ContentView.swift
+++ play1/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Hello! My name is Josep Subils. This is my personal web page. I am a full stack professional developer and programmer in JavaScript. Other experiences: Rust, Go, C#, Java, Ruby, PHP and Perl. Here you can find my personal projects and documentation.")
+            .fontWeight(.regular)
+            .font(.custom("Gill Sans", size: 26))
+            .foregroundColor(.blue)
+            .multilineTextAlignment(.center)
+        .lineLimit(3)
+            .truncationMode(.middle)
     }
 }
 
